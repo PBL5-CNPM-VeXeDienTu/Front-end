eb6cacae3c69793bd82264a58ec0524acdc82234
HoThiHieu <hothihieu2404@gmail.com>
2022-03-22 22:56:15 +0700

    [Frontend][UI] Xây dựng giao diện Login, Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
+import Login from './pages/login';
+import Register from './pages/register'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
+     <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route exact path='/register' element={<Register/>}/>
+        </Routes>
+     </BrowserRouter>
     </div>
   );
 }
