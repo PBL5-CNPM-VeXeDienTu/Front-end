4086c30005cb09370c665b525beff7cea349ecf1
HoThiHieu <hothihieu2404@gmail.com>
2022-04-03 00:54:54 +0700

    [Frontend][UI] Thiết kế lại component của Layout (Siderbar)
    https://trello.com/c/0LWWITXF/5-frontendui-thi%E1%BA%BFt-k%E1%BA%BF-l%E1%BA%A1i-component-c%E1%BB%A7a-layout

--- src/views/pages/profile/index.js
+++ src/views/pages/profile/index.js
@@ -53,41 +53,8 @@ function Profile() {
                             </tr>
                         </table>
                     </div>
-
-                    {/* <div className="content-user-infor">
-                            <div className="user-infor-name">
-                                <div className="user-infor-name-lable">Tên</div>
-                                <div className='user-infor-name-input'>Phạm Văn Thọ</div>
-                            </div>
-                            <div className="user-infor-email">
-                                <div className="user-infor-email-lable">Email</div>
-                                <div className='user-infor-email-input'>Thopham.21082001@gmail.com</div>
-                            </div>
-                            <div className="user-infor-gender">
-                                <div className="user-infor-gender-lable">Giới tính</div>
-                                <div className='user-infor-email-input-item'>Nam</div>
-                            
-                            </div>
-                            <div className="user-infor-birthday">
-                                <div className="user-infor-birthday-lable">Ngày sinh</div>
-                                <div className='user-infor-birthday-input-day'>31/08/2001</div>
-                                <div className='user-infor-birthday-input-icon'>
-                                      <ScheduleOutlined />
-                                </div>
-                            </div>
-                            <div className="user-infor-address">
-                                <div className="user-infor-address-lable">Địa chỉ</div>
-                                <div className='user-infor-address-input'>Tổ 3 , thôn Đại la, xã Hòa Sơn, huyện Hòa vang , TP Đà Nẵng</div>
-                            </div>
-                            <div className="user-infor-phone">
-                                <div className="user-infor-phone-lable">Số điện thoại</div>
-                                <div className='user-infor-phone-input'>0702399134</div>
-                            </div>
-                    </div> */}
                 </div>
             </div>
-            {/* </Layout_basic.Main> */}
-            {/* </Layout_basic> */}
         </div>
     );
 }
