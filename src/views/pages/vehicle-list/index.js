baae944da6b2cdbd17a26dfc6c3233ef5ba67fd4
nguyenhoangphua6 <93857556+nguyenhoangphua6@users.noreply.github.com>
2022-05-25 18:47:51 +0700

    [FrontEnd][API][basic user][vehicles-list] Gọi API cho màn vehicles-list (dành cho basic user)

--- src/views/pages/vehicle-list/index.js
+++ src/views/pages/vehicle-list/index.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { PlusCircleOutlined } from '@ant-design/icons'
 
+import useAuth from 'hooks/useAuth'
+import vehicleApi from 'api/vehicleApi'
 import './vehicle-list.scss'
 
 function Vehicles() {
+    const { user } = useAuth()
+    const [vehicleList, setVehicalList] = useState([])
+
+    useEffect(() => {
+        if (!!user) {
+            vehicleApi.getByUserId(user.id).then((response) => {
+                setVehicalList(response.data)
+            })
+        }
+    }, [user])
+
     const navigate = useNavigate()
     const onClickHandler = () => navigate('/vehicles/detail')
-
-    const avatarURL =
-        process.env.REACT_APP_API_URL +
-        'public/images/avatars/vehicle/default-avatar.png'
 
     return (
         <div className="vehicles-list-container">
@@ -23,109 +32,47 @@ function Vehicles() {
                 </Link>
             </div>
             <div className="vehicles-list-container__content">
-                <div
-                    className="vehicles-list-container__content__sub"
-                    onClick={onClickHandler}
-                >
-                    <div className="vehicles-list-container__content__item">
-                        <img
-                            className="vehicles-list-container__content__item__image"
-                            src={avatarURL}
-                            alt=""
-                        />
-                        <div className="vehicles-list-container__content__item__info">
-                            <div>
-                                <span className="properties">Biển số</span>
-                                <span>123456</span>
-                            </div>
-                            <div>
-                                <span className="properties">Hãng xe</span>
-                                <span>Suzuki</span>
-                            </div>
-                            <div>
-                                <span className="properties">Màu</span>
-                                <span>Xanh đen</span>
-                            </div>
-                            <div>
-                                <span className="properties">Ngày đăng ký</span>
-                                <span>01/01/2022</span>
-                            </div>
-                            <div>
-                                <span className="properties">Xác thực</span>
-                                <span>Đã xác thực</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div
-                    className="vehicles-list-container__content__sub"
-                    onClick={onClickHandler}
-                >
-                    <div className="vehicles-list-container__content__item">
-                        <img
-                            className="vehicles-list-container__content__item__image"
-                            src={avatarURL}
-                            alt=""
-                        />
-                        <div className="vehicles-list-container__content__item__info">
-                            <div>
-                                <span className="properties">Biển số</span>
-                                <span>123456</span>
-                            </div>
-                            <div>
-                                <span className="properties">Hãng xe</span>
-                                <span>Suzuki</span>
-                            </div>
-                            <div>
-                                <span className="properties">Màu</span>
-                                <span>Xanh đen</span>
-                            </div>
-                            <div>
-                                <span className="properties">Ngày đăng ký</span>
-                                <span>01/01/2022</span>
-                            </div>
-                            <div>
-                                <span className="properties">Xác thực</span>
-                                <span>Đã xác thực</span>
+                {vehicleList?.map((vehicle) => (
+                    <div
+                        className="vehicles-list-container__content__sub"
+                        onClick={onClickHandler}
+                    >
+                        <div className="vehicles-list-container__content__item">
+                            <img
+                                className="vehicles-list-container__content__item__image"
+                                src={
+                                    process.env.REACT_APP_API_URL +
+                                    vehicle.avatar
+                                }
+                                alt=""
+                            />
+                            <div className="vehicles-list-container__content__item__info">
+                                <div>
+                                    <span className="properties">Biển số</span>
+                                    <span>{vehicle.license_plate}</span>
+                                </div>
+                                <div>
+                                    <span className="properties">Hãng xe</span>
+                                    <span>{vehicle.brand}</span>
+                                </div>
+                                <div>
+                                    <span className="properties">Màu</span>
+                                    <span>{vehicle.color}</span>
+                                </div>
+                                <div>
+                                    <span className="properties">
+                                        Ngày đăng ký
+                                    </span>
+                                    <span>{vehicle.createdAt}</span>
+                                </div>
+                                <div>
+                                    <span className="properties">Xác thực</span>
+                                    <span>{vehicle.verify_state_id}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div
-                    className="vehicles-list-container__content__sub"
-                    onClick={onClickHandler}
-                >
-                    <div className="vehicles-list-container__content__item">
-                        <img
-                            className="vehicles-list-container__content__item__image"
-                            src={avatarURL}
-                            alt=""
-                        />
-                        <div className="vehicles-list-container__content__item__info">
-                            <div>
-                                <span className="properties">Biển số</span>
-                                <span>123456</span>
-                            </div>
-                            <div>
-                                <span className="properties">Hãng xe</span>
-                                <span>Suzuki</span>
-                            </div>
-                            <div>
-                                <span className="properties">Màu</span>
-                                <span>Xanh đen</span>
-                            </div>
-                            <div>
-                                <span className="properties">Ngày đăng ký</span>
-                                <span>01/01/2022</span>
-                            </div>
-                            <div>
-                                <span className="properties">Xác thực</span>
-                                <span>Đã xác thực</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
